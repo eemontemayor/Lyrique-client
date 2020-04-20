12bfafa8a79b61ecaebc6e82cf5b39d63be05f12
eemontemayor <eemontemayor@live.com>
2020-04-20 15:05:37 -0500

    moved axios req to backend and hid api key... axios req for rhymes  is successful

--- src/services/word-service.js
+++ src/services/word-service.js
@@ -1,26 +1,30 @@
 
 import config from '../config';
 // import TokenService from './TokenService';
- const axios = require("axios");
+
 
  const WordService = {
     getRhymes(word){
 
-      return  axios({
-            "method":"GET",
-            "url":`https://wordsapiv1.p.rapidapi.com/words/%7B${word}%7D/rhymes`,
-            "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"wordsapiv1.p.rapidapi.com",
-            "x-rapidapi-key":"b5f70bd1c9msh133a4bbdfb3c433p1463c0jsn8429fb0c932c"
-            }
-            })
-            .then((response)=>{
-              console.log(Object(response.data))
-            })
-            .catch((error)=>{
-              console.log(error)
-            })
+  console.log('get rhymes')
+      return fetch(`${config.API_ENDPOINT}/word/rhymes/${word}`,{
+        method:'GET',
+        headers:{
+          'content-type':'application/json',
+          // 'authorization':`bearer ${TokenService.getAuthToken()}`,
+        },
+  
+
+
+      })
+      .then((res) => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .catch(error => {
+        console.error({error})
+      })
     }
 
 

9ca2a6e5c99bf0bc16024383893eb3e5c1a8186d
eemontemayor <eemontemayor@live.com>
2020-06-30 17:23:03 -0500

    refactored fetch function on click word

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
 
     syllables: [],
     word: "",
+    resultType: "",
     results: [[], [], []],
   };
   handleChange = (e) => {
@@ -26,17 +27,14 @@ class App extends React.Component {
 
     this.setState({
       word: this.state.searchWord,
-      results:[[],[],[]]
+      results: [[], [], []],
     });
 
-    WordService.getWordData(this.state.searchWord)
-    .then((res) => {
+    WordService.getWordData(this.state.searchWord).then((res) => {
       const { defs, word } = res[0];
 
-     
-
       const pronunciation = res[0].tags[1].slice(4).replace(/\d+/g, "");
-    
+
       this.setState({
         defs,
         word,
@@ -44,39 +42,28 @@ class App extends React.Component {
       });
     });
 
-    WordService.getRhymes(this.state.searchWord)
-    .then((res) => {
+    WordService.getRhymes(this.state.searchWord).then((res) => {
       this.setState({
         results: res,
       });
     });
   };
 
-
-
-
-
-  handleGetAllits = () => {
+  handleClickType = (type) => {
+    const resType = type || "rhymes";
+    console.log("type", resType);
     this.setState(
       {
-        results: [[]],
-      },
-      () => {
-        WordService.getAlliterations(this.state.word).then((res) => {
-          this.setState({
-            results: res,
-          });
-        });
+        resultType: resType,
       }
+  
     );
   };
-
-
 
   handleGetSyns = () => {
     this.setState(
       {
-        results: [],
+        results: [[], [], []],
       },
       () => {
         WordService.getSynonyms(this.state.word).then((res) => {
@@ -91,7 +78,7 @@ class App extends React.Component {
   handleGetRhymes = () => {
     this.setState(
       {
-        results: [],
+        results: [[], [], []],
       },
       () => {
         WordService.getRhymes(this.state.word).then((res) => {
@@ -114,8 +101,14 @@ class App extends React.Component {
         let def = item.slice(item.search(regEx));
 
         return (
-          <li style={{ fontSize: "14px" }} key={index}>
-            {type}:{def}
+          <li
+            style={{
+              fontSize: "14px",
+            }}
+            key={index}
+          >
+            {" "}
+            {type}: {def}{" "}
           </li>
         );
       });
@@ -123,49 +116,54 @@ class App extends React.Component {
     }
   };
 
-  handleClickWord = (word) =>{
-
-   
-
+  handleClickWord = (word) => {
     this.setState(
       {
-        results: [],
+        word: word,
+        results: [[], [], []],
       },
       () => {
-
-        WordService.getWordData(word)
-        .then((res) => {
+        WordService.getWordData(word).then((res) => {
           const { defs, word } = res[0];
-    
-      
-    
+
           const pronunciation = res[0].tags[1].slice(4).replace(/\d+/g, "");
-        
+
           this.setState({
             defs,
             word,
             pronunciation,
           });
         });
-    
-        WordService.getRhymes(word)
-        .then((res) => {
-          this.setState({
-            results: res,
+
+        if (this.state.resultType === 'rhymes') {
+          console.log('this.state.resultType', this.state.resultType)
+          WordService.getRhymes(word)
+            .then((res) => {
+              this.setState({
+                results: res,
+              });
+            });
+        } else if (this.state.resultType === 'synonyms') {
+          console.log('this.state.resultType', this.state.resultType)
+
+          WordService.getSynonyms(this.state.word).then((res) => {
+            this.setState({
+              results: res,
+            });
           });
-        });
+        } else if (this.state.resultType === 'alliterations') {
+          console.log('this.state.resultType', this.state.resultType)
 
+          WordService.getAlliterations(this.state.word).then((res) => {
+            this.setState({
+              results: res,
+            });
 
-
-      })
-
-
-  }
-
-
-
-
-
+          })
+        }
+      }
+    );
+  };
 
   render() {
     const { word, pronunciation } = this.state;
@@ -173,9 +171,8 @@ class App extends React.Component {
     return (
       <div className="grid-container">
         <div className="menu-icon">
-          <i className="fas fa-bars header__menu"></i>
+          <i className="fas fa-bars header__menu"> </i>{" "}
         </div>
-
         <header className="header">
           <div className="header__search">
             Search...
@@ -183,85 +180,95 @@ class App extends React.Component {
               classNameName="App__word_form"
               handleChange={this.handleChange}
               handleWordSubmit={this.handleWordSubmit}
-            />
-          </div>
-          <div className="header__avatar">Your face</div>
+            />{" "}
+          </div>{" "}
+          <div className="header__avatar"> Your face </div>{" "}
         </header>
-
         <aside className="sidenav">
           <div className="sidenav__close-icon">
-            <i className="fas fa-times sidenav__brand-close"></i>
-          </div>
+            <i className="fas fa-times sidenav__brand-close"> </i>{" "}
+          </div>{" "}
           <ul className="sidenav__list">
-            <li className="sidenav__list-item">Item One</li>
-            <li className="sidenav__list-item">Item Two</li>
-            <li className="sidenav__list-item">Item Three</li>
-            <li className="sidenav__list-item">Item Four</li>
-            <li className="sidenav__list-item">Item Five</li>
-          </ul>
+            <li className="sidenav__list-item"> Item One </li>{" "}
+            <li className="sidenav__list-item"> Item Two </li>{" "}
+            <li className="sidenav__list-item"> Item Three </li>{" "}
+            <li className="sidenav__list-item"> Item Four </li>{" "}
+            <li className="sidenav__list-item"> Item Five </li>{" "}
+          </ul>{" "}
         </aside>
-
         <main className="main">
           <div className="main-header">
             <div className="main-header__heading">
-              {word}
-              {pronunciation}
+              {" "}
+              {word} {pronunciation}{" "}
             </div>
-
-            <div className="main-header__updates">{this.renderDefs()}</div>
+            <div className="main-header__updates"> {this.renderDefs()} </div>{" "}
           </div>
-
           <div className="main-overview">
-            <button onClick={this.handleGetRhymes}>
+            <button onClick={()=>this.handleClickType('rhymes')}>
               <div className="overviewcard">
-                <div className="overviewcard__icon">Rhymes</div>
-
-                <div className="overviewcard__info">similair sound</div>
-              </div>
-            </button>
-            <button onClick={this.handleGetSyns}>
+                <div className="overviewcard__icon"> Rhymes </div>
+                <div className="overviewcard__info"> similair sound </div>{" "}
+              </div>{" "}
+            </button>{" "}
+            <button onClick={()=>this.handleClickType('synonyms')}>
               <div className="overviewcard">
-                <div className="overviewcard__icon">Synonyms</div>
-
-                <div className="overviewcard__info">similair meaning</div>
-              </div>
-            </button>
-            <button onClick={this.handleGetAllits}>
+                <div className="overviewcard__icon"> Synonyms </div>
+                <div className="overviewcard__info">
+                  {" "}
+                  similair meaning{" "}
+                </div>{" "}
+              </div>{" "}
+            </button>{" "}
+            <button onClick={()=>this.handleClickType('alliterations')}>
               <div className="overviewcard">
-                <div className="overviewcard__icon">Alliterations</div>
-
-                <div className="overviewcard__info">same inital sound/letter</div>
-              </div>
-            </button>
+                <div className="overviewcard__icon"> Alliterations </div>
+                <div className="overviewcard__info">
+                  {" "}
+                  same inital sound / letter{" "}
+                </div>{" "}
+              </div>{" "}
+            </button>{" "}
             {/* <div className="overviewcard">
-        <div className="overviewcard__icon">Overview</div>
+                    <div className="overviewcard__icon">Overview</div>
 
-        <div className="overviewcard__info">Card</div>
-      </div> */}
+                    <div className="overviewcard__info">Card</div>
+                  </div> */}{" "}
           </div>
-
           <div className="main-cards">
             <div className="card">
-              1 syllable
-              <WordList results={this.state.results[0]} handleClickWord={this.handleClickWord} />
+              1 syllable{" "}
+              <WordList
+                results={this.state.results[0]}
+                handleClickWord={this.handleClickWord}
+              />{" "}
+            </div>{" "}
+            <div className="card">
+              2 syllables{" "}
+              <WordList
+                results={this.state.results[1]}
+                handleClickWord={this.handleClickWord}
+              />{" "}
             </div>
             <div className="card">
-              2 syllables
-              <WordList results={this.state.results[1]}  handleClickWord={this.handleClickWord}/>
-            </div>
-
-            <div className="card">
-              3+ syllables
-              <WordList results={this.state.results[2]}  handleClickWord={this.handleClickWord}/>
-            </div>
-          </div>
+              3 + syllables{" "}
+              <WordList
+                results={this.state.results[2]}
+                handleClickWord={this.handleClickWord}
+              />{" "}
+            </div>{" "}
+          </div>{" "}
         </main>
-
-        <Sidebar width={10} height={"100vh"}></Sidebar>
+        <Sidebar width={10} height={"100vh"}>
+          {" "}
+        </Sidebar>{" "}
         <footer className="footer">
-          <div className="footer__copyright">&copy; 2018 MTH</div>
-          <div className="footer__signature">Made with love by pure genius</div>
-        </footer>
+          <div className="footer__copyright"> & copy; 2018 MTH </div>{" "}
+          <div className="footer__signature">
+            {" "}
+            Made with love by pure genius{" "}
+          </div>{" "}
+        </footer>{" "}
       </div>
     );
   }
